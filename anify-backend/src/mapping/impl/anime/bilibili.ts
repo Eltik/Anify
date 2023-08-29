0a4f3c514285d972439853e0aaeaa5bd45ad0941
Eltik <xp10d363@gmail.com>
2023-08-29 18:06:26 -0400

    Bilibili

--- anify-backend/src/mapping/impl/anime/bilibili.ts
+++ anify-backend/src/mapping/impl/anime/bilibili.ts
@@ -8,7 +8,7 @@ export default class Bilibili extends AnimeProvider {
     override id = "bilibili";
     override url = "https://bilibili.com";
 
-    private api = "https://api.bilibili.tv/intl/gateway/web";
+    private api = "https://app.biliintl.com/intl/gateway/v2";
 
     override formats: Format[] = [Format.MOVIE, Format.ONA, Format.OVA, Format.SPECIAL, Format.TV, Format.TV_SHORT];
 
@@ -21,9 +21,9 @@ export default class Bilibili extends AnimeProvider {
     }
 
     override async search(query: string, format?: Format, year?: number): Promise<Result[] | undefined> {
-        const request = await this.request(`${this.api}/v2/search?keyword=${encodeURIComponent(query)}&platform=web&pn=1&ps=20&qid=&s_locale=en_US`);
+        // https://app.biliintl.com/intl/gateway/v2/app/search/type?keyword=${query}&type=7
+        const request = await this.request(`${this.api}/app/search/type?keyword=${encodeURIComponent(query)}&type=7`);
         if (!request.ok) {
-            console.log(await request.text())
             return [];
         }
         const data = await request.json();
@@ -51,124 +51,5 @@ export default class Bilibili extends AnimeProvider {
         });
 
         return results;
-    }
-
-    override async fetchEpisodes(id: string): Promise<Episode[] | undefined> {
-        const episodes: Episode[] = [];
-
-        const req = await (await this.request(`${this.url}${id.includes("-") ? `/anime/${id}` : `/a/${id}`}`)).text();
-
-        const $ = load(req);
-
-        const tempId = $("head > meta[property='og:url']").attr("content")!.split("/").pop()!;
-        const { last_page, data } = await (await this.request(`${this.url}/api?m=release&id=${tempId}&sort=episode_asc&page=1`)).json();
-
-        data.map((item) => {
-            const updatedAt = new Date(item.created_at ?? Date.now()).getTime();
-
-            episodes.push({
-                id: item.id + "-" + id,
-                number: item.episode,
-                title: item.title && item.title.length > 0 ? item.title : "Episode " + item.episode,
-                img: item.snapshot,
-                isFiller: item.filler === 1,
-                hasDub: false,
-                updatedAt,
-            });
-        });
-
-        const pageNumbers = Array.from({ length: last_page - 1 }, (_, i) => i + 2);
-
-        const promises = pageNumbers.map((pageNumber) => this.request(`${this.url}/api?m=release&id=${tempId}&sort=episode_asc&page=${pageNumber}`).then((res) => res.json()));
-        const results = await Promise.all(promises);
-
-        results.forEach((showData) => {
-            for (const data of showData.data) {
-                if (data) {
-                    const updatedAt = new Date(data.created_at ?? Date.now()).getTime();
-
-                    episodes.push({
-                        id: data.id + "-" + id,
-                        number: data.episode,
-                        title: data.title && data.title.length > 0 ? data.title : "Episode " + data.episode,
-                        img: data.snapshot,
-                        isFiller: data.filler === 1,
-                        hasDub: false,
-                        updatedAt,
-                    });
-                }
-            }
-        });
-        (data as any[]).sort((a, b) => a.number - b.number);
-        return episodes;
-    }
-
-    override async fetchSources(id: string, subType: SubType = SubType.SUB, server: StreamingServers = StreamingServers.Kwik): Promise<Source | undefined> {
-        const animeId = id.split("-").pop()!;
-        const episodeId = id.split("-")[0];
-
-        const req = await this.request(`${this.url}${animeId.includes("-") ? `/anime/${animeId}` : `/a/${animeId}`}`);
-
-        try {
-            const url = req.url;
-            // Need session id to fetch the watch page
-            const sessionId = url.split("/anime/").pop()!;
-
-            const $ = load(await req.text());
-            const tempId = $("head > meta[property='og:url']").attr("content")!.split("/").pop()!;
-            const { last_page, data } = await (await this.request(`${this.url}/api?m=release&id=${tempId}&sort=episode_asc&page=1`)).json();
-
-            let episodeSession = "";
-
-            for (let i = 0; i < data.length; i++) {
-                if (String(data[i].id) === episodeId) {
-                    episodeSession = data[i].session;
-                    break;
-                }
-            }
-
-            if (episodeSession === "") {
-                for (let i = 1; i < last_page; i++) {
-                    const data = await (await this.request(`${this.url}/api?m=release&id=${tempId}&sort=episode_asc&page=${i + 1}`)).json();
-
-                    for (let j = 0; j < data.length; j++) {
-                        if (String(data[j].id) === episodeId) {
-                            episodeSession = data[j].session;
-                            break;
-                        }
-                    }
-
-                    if (episodeSession !== "") break;
-                }
-            }
-
-            if (episodeSession === "") return undefined;
-
-            const watchReq = await (await this.request(`${this.url}/play/${sessionId}/${episodeSession}`)).text();
-
-            const regex = /https:\/\/kwik\.cx\/e\/\w+/g;
-            const matches = watchReq.match(regex);
-
-            if (matches === null) return undefined;
-
-            const result: Source = {
-                sources: [],
-                subtitles: [],
-                intro: {
-                    start: 0,
-                    end: 0,
-                },
-                outro: {
-                    start: 0,
-                    end: 0,
-                },
-                headers: this.headers ?? {},
-            };
-
-            return await new Extractor(matches[0], result).extract(server);
-        } catch (e) {
-            console.error(e);
-            return undefined;
-        }
     }
 }
