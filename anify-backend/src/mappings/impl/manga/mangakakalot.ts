327f5cda082f0493113541c180a78b12b3e4f556
Eltik <76538547+Eltik@users.noreply.github.com>
2023-10-12 12:59:09 -0400

    feat: MangaPill

--- anify-backend/src/mappings/impl/manga/mangakakalot.ts
+++ anify-backend/src/mappings/impl/manga/mangakakalot.ts
@@ -1,6 +1,6 @@
 import MangaProvider from ".";
 import { Format } from "../../../types/enums";
-import { Result } from "../../../types/types";
+import { Chapter, Page, Result } from "../../../types/types";
 import { load } from "cheerio";
 
 export default class Mangakakalot extends MangaProvider {
@@ -11,15 +11,6 @@ export default class Mangakakalot extends MangaProvider {
     override formats: Format[] = [Format.MANGA, Format.ONE_SHOT];
 
     override async search(query: string, format?: Format, year?: number): Promise<Result[] | undefined> {
-        /*
-        const data: SearchResult[] = await (await this.request(`${this.url}/home_json_search`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: `searchword=${query}&searchstyle=1`,
-        })).json();
-        */
         const temp = await (
             await this.request(`${this.url}/home_json_search`, {
                 method: "POST",
@@ -47,8 +38,8 @@ export default class Mangakakalot extends MangaProvider {
 
             promises.push(
                 new Promise(async (resolve, reject) => {
-                    const id = result.story_link.split("/")[3];
-                    const url = id.includes("read") ? this.url : "https://readmanganato.com";
+                    const id = result.story_link.startsWith(this.url) ? result.story_link.split(this.url)[1].slice(1) : result.story_link.split("/")[3];
+                    const url = id.includes("manga/") ? this.url : "https://readmanganato.com";
 
                     const data = await (await this.request(`${url}/${id}`)).text();
                     const $ = load(data);
@@ -75,6 +66,65 @@ export default class Mangakakalot extends MangaProvider {
         await Promise.all(promises);
 
         return results;
+    }
+
+    override async fetchChapters(id: string): Promise<Chapter[] | undefined> {
+        const chapters: Chapter[] = [];
+
+        if (id.includes("manga/")) {
+            const data = await (await this.request(`${this.url}/${id}`)).text();
+            const $ = load(data);
+
+            $("div.chapter-list > div.row")
+                .toArray()
+                .reverse()
+                .map((el, i) => {
+                    chapters.push({
+                        id: $(el).find("span > a").attr("href")?.split("chapter/")[1]!,
+                        title: $(el).find("span > a").text(),
+                        number: i + 1,
+                        rating: null,
+                        updatedAt: $(el).find("span:nth-child(3)").attr("title") ? new Date($(el).find("span:nth-child(3)").attr("title")!).getTime() : undefined,
+                    });
+                });
+        } else {
+            const data = await (await this.request(`https://readmanganato.com/${id}`)).text();
+            const $ = load(data);
+
+            $("div.container-main-left > div.panel-story-chapter-list > ul > li")
+                .toArray()
+                .reverse()
+                .map((el, i) => {
+                    chapters.push({
+                        id: $(el).find("a").attr("href")?.split(".com/")[1]! + "$$READMANGANATO",
+                        title: $(el).find("a").text(),
+                        number: i + 1,
+                        rating: null,
+                        updatedAt: $(el).find("span.chapter-time.text-nowrap").attr("title") ? new Date($(el).find("span.chapter-time.text-nowrap").attr("title")!).getTime() : undefined,
+                    });
+                });
+        }
+
+        return chapters;
+    }
+
+    override async fetchPages(id: string): Promise<string | Page[] | undefined> {
+        const url = !id.includes("$$READMANGANATO") ? `${this.url}/chapter/${id}` : `https://readmanganato.com/${id.replace("$$READMANGANATO", "")}`;
+        const data = await (await this.request(url)).text();
+
+        const $ = load(data);
+
+        const pages = $("div.container-chapter-reader > img")
+            .map(
+                (i, el): Page => ({
+                    url: $(el).attr("src")!,
+                    index: i,
+                    headers: { Referer: this.url },
+                }),
+            )
+            .get();
+
+        return pages;
     }
 }
 
---
+++ anify-backend/src/mappings/impl/manga/mangapill.ts
@@ -0,0 +1,72 @@
+import MangaProvider from ".";
+import { Format } from "../../../types/enums";
+import { Chapter, Page, Result } from "../../../types/types";
+import { load } from "cheerio";
+
+export default class MangaPill extends MangaProvider {
+    override rateLimit = 250;
+    override id = "mangapill";
+    override url = "https://mangapill.com";
+
+    override formats: Format[] = [Format.MANGA, Format.ONE_SHOT];
+
+    override async search(query: string, format?: Format, year?: number): Promise<Result[] | undefined> {
+        const results: Result[] = [];
+
+        const data = await (await this.request(`${this.url}/search?q=${encodeURIComponent(query)}`)).text();
+        const $ = load(data);
+
+        $("div.container div.my-3.justify-end > div").map((i, el) => {
+            results.push({
+                id: $(el).find("a").attr("href")?.split("/manga/")[1]!,
+                title: $(el).find("div > a > div.mt-3").text().trim(),
+                altTitles: $(el).find("div > a > div.text-xs.text-secondary").text().trim() ? [$(el).find("div > a > div.text-xs.text-secondary").text().trim()] : [],
+                img: $(el).find("a img").attr("data-src") ?? "",
+                year: Number.isNaN(Number($($(el).find("div > div.flex > div").find("div")[1]).text().trim())) ? 0 : Number($($(el).find("div > div.flex > div").find("div")[1]).text().trim()),
+                format: Format.UNKNOWN,
+                providerId: this.id,
+            });
+        });
+
+        return results;
+    }
+
+    override async fetchChapters(id: string): Promise<Chapter[] | undefined> {
+        const chapters: Chapter[] = [];
+
+        const data = await (await this.request(`${this.url}/manga/${id}`)).text();
+        const $ = load(data);
+
+        $("div.container div.border-border div#chapters div.grid-cols-1 a").map((i, el) => {
+            chapters.push({
+                id: $(el).attr("href")?.split("/chapters/")[1]!,
+                title: $(el).text().trim(),
+                number: Number.isNaN(Number($(el).text().split("Chapter ")[1])) ? i : Number($(el).text().split("Chapter ")[1]),
+                rating: null,
+            });
+        });
+
+        return chapters;
+    }
+
+    override async fetchPages(id: string): Promise<string | Page[] | undefined> {
+        const pages: Page[] = [];
+
+        const data = await (await this.request(`${this.url}/chapters/${id}`)).text();
+        const $ = load(data);
+
+        const chapterSelector = $("chapter-page");
+
+        chapterSelector.map((i, el) => {
+            pages.push({
+                url: $(el).find("div picture img").attr("data-src")!,
+                index: Number($(el).find("div[data-summary] > div").text().split("page ")[1]?.split("/")[0] ?? i),
+                headers: {
+                    Referer: this.url,
+                },
+            });
+        });
+
+        return pages;
+    }
+}
