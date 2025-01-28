fd2167fcd159f6cdb53e2feb902e175b30d6cac4
Mohtasim Alam Sohom <94981761+shimizudev@users.noreply.github.com>
2025-01-28 11:22:19 +0600

    fix: add routes to index.ts

--- anify-backend/src/app/impl/routes/index.ts
+++ anify-backend/src/app/impl/routes/index.ts
@@ -2,10 +2,22 @@ import stats from "./impl/stats";
 import seasonal from "./impl/seasonal";
 import metadata from "./impl/metadata";
 import info from "./impl/info";
+import episodes from "./impl/episodes";
+import media from "./impl/media";
+import relations from "./impl/relations";
+import search from "./impl/search";
+import similar from "./impl/similar";
+import searchAdvanced from "./impl/searchAdvanced";
 
 export default {
     stats,
     seasonal,
     metadata,
     info,
+    episodes,
+    media,
+    relations,
+    search,
+    similar,
+    searchAdvanced,
 };
