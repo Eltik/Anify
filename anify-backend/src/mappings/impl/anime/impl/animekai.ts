40f07b59321dd94edb69f80aa73b9bc6c92586bf
Mohtasim Alam Sohom <94981761+shimizudev@users.noreply.github.com>
2025-02-04 11:29:14 +0600

    move extractor

--- anify-backend/src/mappings/impl/anime/impl/animekai.ts
+++ anify-backend/src/mappings/impl/anime/impl/animekai.ts
@@ -2,7 +2,7 @@ import { load } from "cheerio";
 import AnimeProvider from "..";
 import { type ISource, StreamingServers, SubType } from "../../../../types/impl/mappings/impl/anime";
 import {type IEpisode, type IProviderResult, MediaFormat } from "../../../../types";
-import AnimekaiDecoder from "../../../../helper/impl/animekai-decoder";
+import AnimekaiDecoder from "../../../../video-extractors/impl/animekai-decoder";
 
 const decoder = AnimekaiDecoder
 
